ab6729d707c2bdf62be2260ff4d736d552b8d6d0
Ramasee <Ramasee@yahoo.com>
2022-05-09 09:07:13 -0400

    Update data.ts

--- src/trees/Engineer/data.ts
+++ src/trees/Engineer/data.ts
@@ -289,6 +289,7 @@ export const data: TalentData = {
         maxRank: 1,
         reqPoints: 15,
         description: talentText`Your range is increased by 20%.`,
+      },
       "Ancestral Inheritance": {
         name: "Archetype Tactic",
         pos: "a1",
